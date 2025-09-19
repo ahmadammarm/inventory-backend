e360154514912e63d16a2d3672f110f77e02033d
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-09-19 08:36:20 +0700

    feat(product): add input validate in category and product entiites

--- internal/product/model/productmodel.go
+++ internal/product/model/productmodel.go
@@ -8,18 +8,17 @@ import (
 
 type ProductCategory struct {
 	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
-	Name      string    `gorm:"type:varchar(100)" json:"name"`
+	Name      string    `gorm:"type:varchar(100)" json:"name" validate:"required,min=3"`
 	Products  []Product `gorm:"foreignKey:ProductCategoryID" json:"products"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
 
-
 type Product struct {
 	ID                uuid.UUID       `gorm:"type:uuid;primaryKey" json:"id"`
-	Name              string          `gorm:"type:varchar(100)" json:"name"`
-	Price             float64         `json:"price"`
-	ProductCategoryID uuid.UUID       `gorm:"type:uuid;not null" json:"product_category_id"`
+	Name              string          `gorm:"type:varchar(100)" json:"name" validate:"required,min=3"`
+	Price             float64         `json:"price" validate:"required,gte=0"`
+	ProductCategoryID uuid.UUID       `gorm:"type:uuid;not null" json:"product_category_id" validate:"required"`
 	ProductCategory   ProductCategory `gorm:"foreignKey:ProductCategoryID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"product_category"`
 	CreatedAt         time.Time       `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt         time.Time       `gorm:"autoUpdateTime" json:"updated_at"`
